672c7f0bd7c8d921486dd6443a373eb2e93eb518
Igor Roncevic <ironcev@hotmail.com>
2015-02-11 07:30:28 +0100

    ClarkesThirdLawExceptionStepByStep project added (wwww.thehumbleprogrammer.com/clarkes-third-law-exception-step-by-step).

---
+++ ClarkesThirdLawExceptionStepByStep/Snippets/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Snippets
+{
+    class Program
+    {
+        static void Main()
+        {
+        }
+
+        public class Thisㅤisㅤaㅤperfectlyㅤvalidㅤclassㅤname
+        {
+            public void Andㅤthisㅤisㅤaㅤperfectlyㅤvalidㅤmethodㅤname() { }
+        }
+    }
+}
