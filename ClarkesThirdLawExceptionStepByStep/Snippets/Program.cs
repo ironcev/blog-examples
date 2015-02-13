461aea027682a5cdc65e9f43a809c15c022fbb92
Igor Roncevic <ironcev@hotmail.com>
2015-02-13 22:57:38 +0100

    Final version of the snippets for the "Clarke's Third Law Exception - Step by Step" post.

--- ClarkesThirdLawExceptionStepByStep/Snippets/Program.cs
+++ ClarkesThirdLawExceptionStepByStep/Snippets/Program.cs
@@ -1,14 +1,112 @@
-﻿namespace Snippets
+﻿using System;
+// ReSharper disable InconsistentNaming
+namespace Snippets
 {
     class Program
     {
         static void Main()
         {
+            //new ClarkesThirdLaw().ArthurㅤCㅤClarkeㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ();
+            //new ㅤ().ArthurㅤCㅤClarkeㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ();
+            new ㅤ().ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ();
+        }
+    }
+
+    public class Thisㅤisㅤaㅤperfectlyㅤvalidㅤclassㅤname
+    {
+        public void Andㅤthisㅤisㅤaㅤperfectlyㅤvalidㅤmethodㅤname() { }
+    }
+
+    public class ClarkesThirdLaw
+    {
+        public void Anyㅤsufficientlyㅤadvancedㅤtechnologyㅤisㅤindistinguishableㅤfromㅤmagicㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ()
+        {
+            throw new Exception("Clarke's third law");
         }
 
-        public class Thisㅤisㅤaㅤperfectlyㅤvalidㅤclassㅤname
+        public void ArthurㅤCㅤClarkeㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ()
         {
-            public void Andㅤthisㅤisㅤaㅤperfectlyㅤvalidㅤmethodㅤname() { }
+            Anyㅤsufficientlyㅤadvancedㅤtechnologyㅤisㅤindistinguishableㅤfromㅤmagicㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ();
         }
     }
 }
+
+//public class ㅤ
+//{
+//    public void Anyㅤsufficientlyㅤadvancedㅤtechnologyㅤisㅤindistinguishableㅤfromㅤmagicㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ()
+//    {
+//        throw new Exception("Clarke's third law");
+//    }
+
+//    public void ArthurㅤCㅤClarkeㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ()
+//    {
+//        Anyㅤsufficientlyㅤadvancedㅤtechnologyㅤisㅤindistinguishableㅤfromㅤmagicㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ();
+//    }
+//}
+
+//#pragma warning disable 1709
+//#line 1 ""
+//public class ㅤ
+//{
+//#line 1 ""
+//    public void Anyㅤsufficientlyㅤadvancedㅤtechnologyㅤisㅤindistinguishableㅤfromㅤmagicㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ()
+//    {
+//#line 1 ""
+//        throw new Exception("Clarke's third law");
+//    }
+//#line 1 ""
+//    public void ArthurㅤCㅤClarkeㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ()
+//    {
+//#line 1 ""
+//        Anyㅤsufficientlyㅤadvancedㅤtechnologyㅤisㅤindistinguishableㅤfromㅤmagicㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ();
+//    }
+//}
+//#pragma warning restore 1709
+
+#pragma warning disable 1709
+#line 1 ""
+public class ㅤ
+{
+#line 1 ""
+    public void ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ()
+    {
+#line 1 ""
+        throw new Exception("Clarke's third law");
+    }
+
+#line 1 ""
+    public void ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ()
+    {
+#line 1 ""
+        ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ();
+    }
+
+#line 1 ""
+    public void ㅤㅤㅤAnyㅤsufficientlyㅤadvancedㅤtechnologyㅤisㅤindistinguishableㅤfromㅤmagicㅤㅤㅤㅤㅤㅤㅤ()
+    {
+#line 1 ""
+        ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ();
+    }
+#line 1 ""
+    public void ㅤㅤㅤArthurㅤCㅤClarkeㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ()
+    {
+#line 1 ""
+        ㅤㅤㅤAnyㅤsufficientlyㅤadvancedㅤtechnologyㅤisㅤindistinguishableㅤfromㅤmagicㅤㅤㅤㅤㅤㅤㅤ();
+    }
+
+#line 1 ""
+    public void ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ()
+    {
+#line 1 ""
+        ㅤㅤㅤArthurㅤCㅤClarkeㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ();
+    }
+
+#line 1 ""
+    public void ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ()
+    {
+#line 1 ""
+        ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ();
+    }
+}
+#pragma warning restore 1709
+// ReSharper restore InconsistentNaming
