fc13d59bfee66bdb14313de5e9f989b9d62772d7
Igor Rončević <ironcev@hotmail.com>
2018-03-21 23:06:05 +0100

    async-as-async: Adds the ContextualKeywordVar example

---
+++ AwaitAsyncAsAsync/Snippets/ContextualKeywordVar.cs
@@ -0,0 +1,17 @@
+﻿namespace SnippetsButAgainABitDifferentToAvoidNameClashing
+{
+    class ContextualKeywordVar
+    {
+        int var() => 0;
+        int var(int var) => 0;
+
+        void VarIdentifierAndContextualKeyword()
+        {
+            int a = this.var();
+            var b = 0;
+            int var = 0;
+            var = this.var(var);
+            int.TryParse("123", out var result);
+        }
+    }
+}
