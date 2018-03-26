29be35761a00f48e4f2cf2bafbfb4c7bf7089000
Igor Rončević <ironcev@hotmail.com>
2018-03-26 23:00:31 +0200

    variform: Adds the ADynamicHomework example

---
+++ TheVariformVar/Snippets/ADynamicHomework.cs
@@ -0,0 +1,15 @@
+﻿class dynamic
+{
+    public static implicit operator dynamic(string text)
+    {
+        return null;
+    }
+}
+
+class ADynamicHomework
+{
+    public void Method()
+    {
+        dynamic dynamic = "dynamic";
+    }
+}
